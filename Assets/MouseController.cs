f3e6e05b4e0ab387c0865f0d5ad5ad9851ea3727
nickocaluya <nickocaluya@gmail.com>
2018-05-21 13:40:55 +0900

    Basic Demo - First Commit
    + Simple demo with annotated and unannotated 360 images
    + Builds only as standalone app, so far

---
+++ Assets/MouseController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour {
+
+	public float speed = 3.5f;
+	private float X;
+	private float Y;
+
+	void Update() {
+		if(Input.GetMouseButton(0)) {
+			transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * speed, -Input.GetAxis("Mouse X") * speed, 0));
+			X = transform.rotation.eulerAngles.x;
+			Y = transform.rotation.eulerAngles.y;
+			transform.rotation = Quaternion.Euler(X, Y, 0);
+		}
+	}
+}
